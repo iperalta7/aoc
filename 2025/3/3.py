8d34ae01fbb3ea16c8bfc2d9cf42be327d2dddc5
Isaiah Peralta <98777083+iperalta7@users.noreply.github.com>
2025-12-03 23:27:30 -0500

    day 3 part 2

--- 2025/3/3.py
+++ 2025/3/3.py
@@ -18,7 +18,37 @@ def part1():
     sum = 0
     for bank in banks:
         sum += find_biggest_num(bank)
+    return sum
 
-    print(sum)
+# this code can be cleaned up
+def big_num_window(bank):
+    nums = []
+    current_window_size = 12
+    window = bank[:len(bank) - 11]
 
-part1()
+    prev_left_idx = 0
+    for i in range(12):
+        next_max_num = max(window)
+        nums.append(next_max_num)
+
+        # enforce correct new left shift
+        new_left_idx = window.index(next_max_num) + prev_left_idx + 1
+
+        #  enforce remaining window size
+        remaining = 12 - len(nums) + 1
+        new_right_idx = len(bank) - remaining + 1
+
+        window = bank[new_left_idx:new_right_idx]
+        prev_left_idx = new_left_idx
+
+    return int(''.join(map(str, nums)))
+
+def part2():
+    sum = 0
+    for bank in banks: # 
+        num = big_num_window(bank)
+        sum += num
+    return sum
+
+print(f"Part 1: {part1()}")
+print(f"Part 2: {part2()}")
